a14a30f5fefba7e469523ae0cc26630c69de328b
Cloufield <610935659@qq.com>
2022-12-23 11:57:20 +0900

    added qc

---
+++ 4_Data_QC/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#$ -S /bin/bash
+export PATH=~/tools/bin:$PATH
+export OMP_NUM_THREADS=1
+
+genotypeFile="../1_Dataset/1KG.EAS.auto.snp.norm.nodup.split.maf005.thinp020"
+
+plink \
+        --bfile ${genotypeFile} \
+        --missing \
+        --freq \
+        --hardy \
+        --het \
+        --out plink_results
+
+
+plink \
+        --bfile ${genotypeFile} \
+        --maf 0.01 \
+        --geno 0.01 \
+        --mind 0.02 \
+        --hwe 5e-6 \
+        --indep-pairwise 50 5 0.2 \
+        --out plink_results
+
+plink \
+        --bfile ${genotypeFile} \
+        --extract plink_results.prune.in \
+        --genome \
+        --out plink_results
