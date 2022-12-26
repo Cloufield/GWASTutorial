36152c17cd868adc61337fddd22228b377769699
Cloufield <610935659@qq.com>
2022-12-26 10:57:06 +0900

    init docs

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+for dir in 01_Dataset 02_Linux_basics 03_Data_formats 04_Data_QC 05_PCA 06_Association_tests 07_Annotation 08_LDSC 09_Gene_based_analysis 10_PRS 80_anaconda 81_jupyter_notebook 82_windows_linux_subsystem 83_git_and_github
+do
+cp ${dir}/README.md docs/${dir}.md
+done
+cp README.md docs/index.md
+mkdocs gh-deploy
