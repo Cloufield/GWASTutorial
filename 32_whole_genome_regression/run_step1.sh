0fe1fd9c7600550d9d44ae4e65f7acea78ee1582
Cloufied <610935659@qq.com>
2023-01-02 22:00:23 +0900

    added regenie

---
+++ 32_whole_genome_regression/run_step1.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+plinkFile=../01_Dataset/1KG.EAS.auto.snp.norm.nodup.split.maf005.thinp020
+phenoFile=../01_Dataset/1kgeas_binary_regenie.txt
+covarFile=../05_PCA/plink_results_projected.sscore
+covarList="PC1_AVG,PC2_AVG,PC3_AVG,PC4_AVG,PC5_AVG,PC6_AVG,PC7_AVG,PC8_AVG,PC9_AVG,PC10_AVG"
+extract=../05_PCA/plink_results.prune.in
+
+sed -i 's/#FID/FID/' ../05_PCA/plink_results_projected.sscore
+mkdir tmpdir
+
+regenie \
+  --step 1 \
+  --bed ${plinkFile} \
+  --extract ${extract} \
+  --phenoFile ${phenoFile} \
+  --covarFile ${covarFile} \
+  --covarColList ${covarList} \
+  --bt \
+  --bsize 1000 \
+  --lowmem \
+  --lowmem-prefix tmpdir/regenie_tmp_preds \
+  --out 1kg_eas_step1_BT
