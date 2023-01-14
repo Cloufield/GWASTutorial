1f6c877ad9ea9cf44421ff1a68a5c03f57e6b92e
Cloufied <610935659@qq.com>
2023-01-14 13:41:17 +0900

    update

--- 12_fine_mapping/README.md
+++ 12_fine_mapping/README.md
@@ -1,4 +1,4 @@
-# Fine-mapping
+# Fine-mapping (under construction)
 
 ## Introduction
 
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 mkdir -p docs
-for dir in 96_Assignment2 95_Assignment 33_linear_mixed_model 32_whole_genome_regression 99_About 69_resources 13_heritability 85_job_scheduler 84_ssh 90_Recommended_Reading 01_Dataset 02_Linux_basics 03_Data_formats 04_Data_QC 05_PCA 06_Association_tests 07_Annotation 08_LDSC 09_Gene_based_analysis 10_PRS 60_awk 61_sed 80_anaconda 81_jupyter_notebook 82_windows_linux_subsystem 83_git_and_github
+for dir in 12_fine_mapping 96_Assignment2 95_Assignment 33_linear_mixed_model 32_whole_genome_regression 99_About 69_resources 13_heritability 85_job_scheduler 84_ssh 90_Recommended_Reading 01_Dataset 02_Linux_basics 03_Data_formats 04_Data_QC 05_PCA 06_Association_tests 07_Annotation 08_LDSC 09_Gene_based_analysis 10_PRS 60_awk 61_sed 80_anaconda 81_jupyter_notebook 82_windows_linux_subsystem 83_git_and_github
 do
 cp ${dir}/README.md docs/${dir}.md
 done
