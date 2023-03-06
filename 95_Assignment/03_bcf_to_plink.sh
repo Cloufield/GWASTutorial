c7e7f60143d59679c4e9768a3bdb1f2e98c67b8d
Maomao Shi <84720165+shimaomao26@users.noreply.github.com>
2023-03-06 23:40:19 +0900

    Update 03_bcf_to_plink.sh
    add a directory

--- 95_Assignment/03_bcf_to_plink.sh
+++ 95_Assignment/03_bcf_to_plink.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+mkdir -p 1kg-plink
+
 for chr in {1..22}
 do
 plink \
-      --bcf ALL.chr"${chr}".phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.bcf \
+      --bcf 1kg-norm-nodup/ALL.chr"${chr}".phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.bcf \
       --keep-allele-order \
       --vcf-idspace-to _ \
       --const-fid \
       --allow-extra-chr 0 \
       --split-x b37 no-fail \
       --make-bed \
-      --out ALL.chr"${chr}".phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes 
+      --out 1kg-plink/ALL.chr"${chr}".phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes 
 done
