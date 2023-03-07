d9178ad1a44403677eb1f37b21a7cce41db11979
Maomao Shi <84720165+shimaomao26@users.noreply.github.com>
2023-03-07 13:50:15 +0900

    Update 02_normalize_remove_dup.sh
    erase the rsid first and then add new id.

--- 95_Assignment/02_normalize_remove_dup.sh
+++ 95_Assignment/02_normalize_remove_dup.sh
@@ -6,7 +6,7 @@ for chr in {1..22}
 do
     bcftools norm -m-any --check-ref w -f 1kg/human_g1k_v37.fasta \
       1kg/ALL.chr"${chr}".phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz | \
-      bcftools annotate -I +'%CHROM:%POS:%REF:%ALT' | \
+      bcftools annotate -x ID -I +'%CHROM:%POS:%REF:%ALT' | \
         bcftools norm -Ob --rm-dup both \
           > 1kg-norm-nodup/ALL.chr"${chr}".phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.bcf ;
 
