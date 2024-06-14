90021b3866cbe75f01390a0e6850add3f752910e
Yunye <610935659@qq.com>
2024-06-14 14:24:11 +0900

    update

--- 07_Annotation/1_annovar.sh
+++ 07_Annotation/1_annovar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 input=annovar_input.txt
-humandb=/home/he/tools/annovar/annovar/humandb
+humandb=~/tools/annovar/annovar/humandb
 table_annovar.pl ${input} ${humandb} -buildver hg19 -out myannotation -remove -protocol refGene -operation g -nastring . -polish
